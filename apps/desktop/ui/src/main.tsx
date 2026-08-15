8f4b0353db2f93bf7f822d043b2c792d55b88945
Aryan Gupta <aassworld99@gmail.com>
2026-08-15 09:14:39 +0530

    feat(ui): switch desktop shell to NOMAD stable v3

--- apps/desktop/ui/src/main.tsx
+++ apps/desktop/ui/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppV3 from "./AppV3";
 import "./styles.css";
+import "./AppV3.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppV3 />
   </React.StrictMode>,
 );
