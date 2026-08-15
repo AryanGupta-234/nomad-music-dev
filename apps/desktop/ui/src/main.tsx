49f4f6482502b3b7da3e8f24d1f87bcff3635bf9
Aryan Gupta <aassworld99@gmail.com>
2026-08-15 19:34:26 +0530

    feat(ui): switch desktop app to compact stable workspace

--- apps/desktop/ui/src/main.tsx
+++ apps/desktop/ui/src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppStable from "./AppStable";
 import "./styles.css";
 import "./nomad-polish.css";
+import "./AppStable.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppStable />
   </React.StrictMode>,
 );
